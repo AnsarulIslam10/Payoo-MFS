b076f63762e69a61c3c545c700c74c06aa754c9d
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-09-18 22:17:29 +0600

    partially added the add money event handler

---
+++ js/home.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.defaultPrevented();
+
+    const addMoneyInput = document.getElementById('add-money-input').value;
+
+    const pinNumber = document.getElementById('input-pin-number').value;
+
+})
