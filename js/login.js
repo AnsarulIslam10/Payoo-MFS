fde36d9d8981df248f58ec5931447a4870cf350e
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-09-18 23:53:11 +0600

    rename

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ document.getElementById('button-login').addEventListener('click', function(event
     const pinNumber = document.getElementById('pin-number').value;
 
     if (phoneNumber === '5' && pinNumber === '1234') {
-        window.location.href = '/home/index.html'
+        window.location.href = '/home.html'
     }else{
         alert("Invalid Phone Number or Pin");
     }
